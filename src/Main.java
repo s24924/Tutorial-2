5e796a9b3a5c84732b2537007180ceeaa00f0486
s24924 <s24924@pjwstk.edu.pl>
2025-06-20 09:19:07 +0200

    Task2

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,42 @@ public class Main {
         }
         return resultArray;
     }
+    public static int findMajorityElement(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return -1;
+        }
+
+        int candidate = -1;
+        int count = 0;
+        for (int num : numbers) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        count = 0;
+        for (int num : numbers) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > numbers.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1, 3, 3, 5, 3};
+        int[] arr2 = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(filterEvenIndexOddValue(arr)));
+        System.out.println(findMajorityElement(arr));
+        System.out.println(findMajorityElement(arr2));
     }
 }
